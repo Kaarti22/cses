c4bdb04234d9a7789bff8fd2b923a1cb5570e80a
Kaarti22 <kaartimondi@gmail.com>
2024-10-27 00:46:04 +0530

    permutations

---
+++ Introductory Problems/Permutations.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(int n){
+        if(n >= 2 && n <= 3){
+            cout<<"NO SOLUTION"<<endl;
+            return;
+        }
+        if(n == 4){
+            cout<<"3 1 4 2"<<endl;
+            return;
+        }
+        vi v(n);
+        int i = 0;
+        int curr = 1;
+        while(i < n){
+            v[i] = curr;
+            curr++;
+            i += 2;
+        }
+        i = 1;
+        while(i < n){
+            v[i] = curr;
+            curr++;
+            i += 2;
+        }
+        disvec(v, n);
+    }
+};
+
+int32_t main(){
+    int n;
+    cin>>n;
+    Kaarti *ob = new Kaarti;
+    ob->method(n);
+    return 0;
+}
