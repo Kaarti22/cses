340a8b3a8709bfec7c129a8d87d78a5909198d87
Kaarti22 <kaartimondi@gmail.com>
2024-10-30 09:32:41 +0530

    stick lengths

---
+++ Sorting and Searching/Stick_Lengths.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(vi &v, int n){
+        sort(all(v));
+        int val;
+        if(n & 1) val = v[n/2];
+        else val = (v[n/2-1] + v[n/2])/2;
+        int ans = 0;
+        for(auto it: v){
+            ans += abs(it - val);
+        }
+        cout<<ans<<endl;
+    }
+};
+
+int32_t main(){
+    int n;
+    cin>>n;
+    vi v(n);
+    inpvec(v, n);
+    Kaarti *ob = new Kaarti;
+    ob->method(v, n);
+    return 0;
+}
