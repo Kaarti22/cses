5b6a6fbf37b529a394d45561d9f476732a477036
Kaarti22 <kaartimondi@gmail.com>
2025-05-25 15:57:28 +0530

    coin combinations 2

---
+++ Dynamic Programming/Coin_Combinations_II.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(vi &v, int n, int x){
+        vi dp(x+1, 0);
+        dp[0] = 1;
+        rep(i,0,n){
+            rep(j,v[i],x+1){
+                dp[j] = madd(dp[j], dp[j-v[i]]);
+            }
+        }
+        cout<<dp[x]<<endl;
+    }
+};
+
+int32_t main(){
+    int n, x;
+    cin>>n>>x;
+    vi v(n);
+    inpvec(v, n);
+    Kaarti ob;
+    ob.method(v, n, x);
+    return 0;
+}
