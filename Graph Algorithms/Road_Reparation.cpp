a6078421905c372b6ecdf584868b6567b075514f
Kaarti22 <kaartimondi@gmail.com>
2025-05-29 11:01:13 +0530

    road reparation

---
+++ Graph Algorithms/Road_Reparation.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class DisjointSet {
+    vector<int> parent, size;
+
+public:
+    DisjointSet(int n){
+        parent.resize(n+1);
+        size.resize(n+1);
+        rep(i,0,n+1){
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node){
+        if(parent[node] == node) return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    int findSize(int node){
+        return size[node];
+    }
+
+    void unionBySize(int u, int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v) return;
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Kaarti{
+public:
+    void method(vvi& v, int n){
+        DisjointSet ds(n);
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        for(auto it: v){
+            pq.push({it[2], {it[0], it[1]}});
+        }
+        int ans = 0;
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+            int wt = it.first, u = it.second.first, v = it.second.second;
+            if(ds.findUPar(u) == ds.findUPar(v)) continue;
+            ans += wt;
+            ds.unionBySize(u, v);
+        }
+        int size = ds.findSize(ds.findUPar(1));
+        if(size == n) cout<<ans<<endl;
+        else cout<<"IMPOSSIBLE"<<endl;
+    }
+};
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m;
+    cin>>n>>m;
+    vvi v(m, vi(3));
+    rep(i,0,m) cin>>v[i][0]>>v[i][1]>>v[i][2];
+    Kaarti ob;
+    ob.method(v, n);
+    return 0;
+}
