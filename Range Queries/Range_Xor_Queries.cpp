1aabd935e5e956eeac65fa9eb8426ecb75cc2794
Kaarti22 <kaartimondi@gmail.com>
2025-09-21 14:06:14 +0530

    range xor queries

---
+++ Range Queries/Range_Xor_Queries.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(vi& v, vvi& q, int n){
+        vi prefixXor(n+1);
+        prefixXor[0] = 0;
+        rep(i,1,n+1){
+            prefixXor[i] = prefixXor[i-1] ^ v[i-1];
+        }
+        for(auto it: q){
+            cout<<(prefixXor[it[1]] ^ prefixXor[it[0]-1])<<endl;
+        }
+    }
+};
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, q;
+    cin>>n>>q;
+    vi v(n);
+    inpvec(v, n);
+    vvi queries(q, vi(2));
+    rep(i,0,q){
+        cin>>queries[i][0]>>queries[i][1];
+    }
+    Kaarti ob;
+    ob.method(v, queries, n);
+    return 0;
+}
