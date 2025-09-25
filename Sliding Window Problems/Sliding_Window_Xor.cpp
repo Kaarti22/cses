966403aa14fb07fbfa75a7eb67e1f75ce1b77d78
Kaarti22 <kaartimondi@gmail.com>
2025-09-25 23:50:37 +0530

    sliding window xor

---
+++ Sliding Window Problems/Sliding_Window_Xor.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(int n, int k, int x, int a, int b, int c){
+        vi v(n);
+        v[0] = x;
+        int ans = 0;
+        if(k == 1) ans ^= x;
+        int currXor = x;
+        int i = 0, j = 1;
+        while(j < n){
+            while(j - i >= k){
+                currXor ^= v[i];
+                i++;
+            }
+            v[j] = (v[j-1] * a + b) % c;
+            currXor ^= v[j];
+            j++;
+            if(j - i == k){
+                ans ^= currXor;
+            }
+        }
+        cout<<ans<<endl;
+    }
+};
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, k;
+    cin>>n>>k;
+    int x, a, b, c;
+    cin>>x>>a>>b>>c;
+    Kaarti ob;
+    ob.method(n, k, x, a, b, c);
+    return 0;
+}
