4aebcc55cd9614a5a70a6bef8c694a0ffe384580
Kaarti22 <kaartimondi@gmail.com>
2025-09-25 10:27:13 +0530

    sliding window sum

---
+++ Sliding Window Problems/Sliding_Window_Sum.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(int n, int k, int x, int a, int b, int c){
+        vi v = {x};
+        int sum = x;
+        int i = 0, j = 1;
+        int ans = 0;
+        if(k == 1) ans ^= sum;
+        while(j < n){
+            while(j - i >= k){
+                sum -= v[i];
+                i++;
+            }
+            int curr = (a * v.back() + b) % c;
+            v.pb(curr);
+            sum += curr;
+            j++;
+            if(j - i == k){
+                ans ^= sum;
+            }
+        }
+        cout<<ans<<endl;
+    }
+};
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, k;
+    cin>>n>>k;
+    int x, a, b, c;
+    cin>>x>>a>>b>>c;
+    Kaarti ob;
+    ob.method(n, k, x, a, b, c);
+    return 0;
+}
