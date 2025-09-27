af0b9cd1db9f96d3831a8e231f561d7317fa8249
Kaarti22 <kaartimondi@gmail.com>
2025-09-27 15:45:23 +0530

    sliding window mex

---
+++ Sliding Window Problems/Sliding_Window_Mex.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(vi& v, int n, int k){
+        set<int> mex;
+        rep(i,0,200001){
+            mex.insert(i);
+        }
+        unordered_map<int, int> m;
+        int i = 0, j = 0;
+        while(j < n){
+            if(j - i == k){
+                m[v[i]]--;
+                if(m[v[i]] == 0){
+                    m.erase(v[i]);
+                    mex.insert(v[i]);
+                }
+                i++;
+            }
+            m[v[j]]++;
+            mex.erase(v[j]);
+            j++;
+            if(j - i == k){
+                cout<<*mex.begin()<<" ";
+            }
+        }
+        cout<<endl;
+    }
+};
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, k;
+    cin>>n>>k;
+    vi v(n);
+    inpvec(v, n);
+    Kaarti ob;
+    ob.method(v, n, k);
+    return 0;
+}
